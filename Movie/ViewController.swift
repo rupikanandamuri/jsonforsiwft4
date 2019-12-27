99b49d3d401734a798f026add89bf419325239e6
rupikanandamuri <rupikachowdhary09@gmail.com>
2019-12-27 11:10:33 -0500

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Rupika on 2019-12-27.
+//  Copyright © 2019 Rupika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
